717fca6d0c7c0aa9ca8c47be8a1f6bccc5392b78
Brett Zamir <brettz9@yahoo.com>
2022-03-30 18:39:41 +0800

    initial commit (Rashh-i-'Ama only)

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  extends: 'ash-nazg/sauron-node-overrides',
+  globals: {
+    document: 'off',
+    fetch: 'off'
+  },
+  settings: {
+    polyfills: [
+      'console',
+      'JSON',
+      'Number.parseInt',
+      'Object.entries',
+      'Object.fromEntries',
+      'Promise'
+    ]
+  },
+  parserOptions: {
+    ecmaVersion: 2021
+  }
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+# bahai-reflib-data
+
+This project aims to host information on the official Bahá'í Reference Library
+website, in particular its unique identifiers, and host scripts for obtaining
+and processing those IDs, such as to map them to paragraph number.
